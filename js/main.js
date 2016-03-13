1a23a8ea451bca0c02fad26da1a459783f94241e
juankboards <juankboards@hotmail.com>
2016-03-12 19:54:21 -0500

    add options funcionality

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	var show = ".about";
+	$(".option-container").on({
+		'click':function(){			
+			var option = $(this).attr("class").split(" ");
+
+			switch(option[1]){
+				case "about-option":
+					$(show).css("display","none");
+					$(".about").css("display", "block");
+					show = ".about";
+					break;
+				case "resume-option":
+					$(show).css("display","none");
+					.content-container{
+					$(".resume").css("display", "block");
+					show = ".resume";
+					break;
+				case "projects-option":
+					$(show).css("display","none");
+					$(".work").css("display", "block");
+					show = ".work";
+					break;
+
+			}
+		}
+	});
+});
